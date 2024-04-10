bce6a93c956cd02811bac2d7ea91c64eac124ac8
brack <angelo.gaming.93@outlook.it>
2024-04-10 04:04:28 +0200

    feat: added game session

---
+++ src/main/java/com/brack/memorygame/socket/model/GameSession.kt
@@ -0,0 +1,40 @@
+package com.brack.memorygame.socket.model
+
+import com.brack.memorygame.gameplay.CellOwner
+import com.brack.memorygame.gameplay.GameBoard
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Sinks
+
+class GameSession(/*val playerSessionA : String*/) {
+    private val gameBoard = GameBoard()
+    val sinkA: Sinks.Many<GameMessage> = Sinks.many().unicast().onBackpressureBuffer()
+    val sinkB: Sinks.Many<GameMessage> = Sinks.many().unicast().onBackpressureBuffer()
+    var available : Boolean = true
+
+    fun getOpponentSink(player: CellOwner) =
+        if (player == CellOwner.PLAYER_A) sinkB else sinkA
+
+    fun getSink(player: CellOwner) =
+        if (player == CellOwner.PLAYER_A) sinkA else sinkB
+
+    fun handleMessage(player: CellOwner, gameMessage: GameMessage) : Flux<GameMessage> {
+        return if (gameMessage.type == MessageType.CLIENT_CLICK) {
+            gameBoard.updateCell(gameMessage.text.toInt(), player)
+            val showFigureMessage = GameMessage(gameMessage.text, MessageType.SHOW_FIGURE)
+            getSink(player).tryEmitNext(showFigureMessage)
+            Flux.just(showFigureMessage)
+        } else {
+            Flux.just(gameMessage)
+        }
+    }
+    //private lateinit var playerSessionB : String
+/*
+    fun accept(sessionId: String) =
+        if (!this::playerSessionB.isInitialized) {
+            playerSessionB = sessionId
+            true
+        } else {
+            false
+        }
+ */
+}
