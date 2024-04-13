78e5a87d40f8b7d256250956a6778376f1e4b112
brack <angelo.gaming.93@outlook.it>
2024-04-13 21:51:17 +0200

    feat: added cell owner opponent utility

--- src/main/java/com/brack/memorygame/gameplay/CellOwner.kt
+++ src/main/java/com/brack/memorygame/gameplay/CellOwner.kt
@@ -3,5 +3,11 @@ package com.brack.memorygame.gameplay
 enum class CellOwner {
     NONE,
     PLAYER_A,
-    PLAYER_B
+    PLAYER_B;
+
+    fun opponent() = when(this) {
+        PLAYER_A -> PLAYER_B
+        PLAYER_B -> PLAYER_A
+        else -> NONE
+    }
 }
