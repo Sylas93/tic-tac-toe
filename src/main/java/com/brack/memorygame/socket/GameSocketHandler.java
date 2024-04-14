16ed57ed7176af148478fc38ad941adec211579a
brack <angelo.gaming.93@outlook.it>
2024-04-14 20:33:43 +0200

    chore: cleanup unused code

--- src/main/java/com/brack/memorygame/socket/GameSocketHandler.java
+++ src/main/java/com/brack/memorygame/socket/GameSocketHandler.java
@@ -1,9 +1,7 @@
 package com.brack.memorygame.socket;
 
-import com.brack.memorygame.gameplay.GameBoard;
 import com.brack.memorygame.socket.model.GameMessage;
 import com.brack.memorygame.socket.model.GameSession;
-import com.brack.memorygame.socket.model.MessageType;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,20 +11,11 @@ import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import static com.brack.memorygame.gameplay.CellOwner.*;
 
 public class GameSocketHandler implements WebSocketHandler {
 
     private Logger logger = LoggerFactory.getLogger(GameSocketHandler.class);
-    private static final String START_MESSAGE =
-            new GameMessage("Waiting...", MessageType.INFO).write();
-
-    private static final GameBoard GAME_BOARD = new GameBoard();
-    private static final Map<GameBoard, List<WebSocketSession>> gameToSocket = new HashMap<>();
 
     @NotNull
     @Override
@@ -50,41 +39,5 @@ public class GameSocketHandler implements WebSocketHandler {
                 .takeUntil(GameMessage::isLast)
                 .map(GameMessage::write).map(session::textMessage)
         );
-        /*
-            var initialization = gameToSocket.entrySet().stream().filter(e -> e.getValue().size() == 1)
-                .findFirst()
-                .map(e -> {
-                    e.getValue().add(session);
-                    return e.getValue().stream().map(s ->
-                        s.send(Mono.just(s.textMessage(
-                            new GameMessage("All client joined", MessageType.INFO).write()
-                        )))
-                    );
-                })
-                .map(Stream::toList)
-                .or(() -> {
-                    var sessions = new ArrayList<WebSocketSession>();
-                    sessions.add(session);
-                    gameToSocket.put(new GameBoard(), sessions);
-                    return java.util.Optional.of(List.of(session.send(Mono.just(session.textMessage(START_MESSAGE)))));
-                })
-                .map(Flux::concat).orElseThrow();
-
-
-
-        Flux<WebSocketMessage> input = session.receive()
-                .map(WebSocketMessage::getPayloadAsText)
-                .map(GameMessage::of)
-                .filter(msg -> msg.getType() == MessageType.CLIENT_CLICK)
-                .map(msg -> {
-                    int index = Integer.parseInt(msg.getText());
-                    GAME_BOARD.updateCell(index, PLAYER_A);
-                    return new GameMessage(msg.getText(), MessageType.SHOW_FIGURE).write();
-                })
-                .map(session::textMessage)
-                .doOnError(ex -> System.out.println(ex.getMessage()));
-
-        return initialization.then(session.send(input));
-        */
     }
 }
