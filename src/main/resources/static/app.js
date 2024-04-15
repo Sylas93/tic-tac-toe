f36bb562bb1b633698b85e41f707c98adef98313
brack <angelo.gaming.93@outlook.it>
2024-04-16 00:36:16 +0200

    feat: improved UI

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,41 +1,28 @@
 const { webSocket } = rxjs.webSocket;
 var socketConnection = webSocket('ws://localhost:8080/socket');
 var figure = "";
-var gameLoaded = false;
-
-$(document).ready(function () {
-    console.log('ready');
-    $( "#target" ).on( "click", function() {
-      alert( "Handler for `click` called." );
-      console.log("hello");
-    } );
-    /*setTimeout(function(){
-      $(document).on('click','#add_external_link',function(){
-          alert('clicked');
-      });
-   },1000);*/
-});
+var gameState = "NO_GAME";
+var errorFlag = false;
 
 function initialize() {
-    if (gameLoaded) return;
-    gameLoaded = true;
-    console.log("Hello")
-    $(".container").append(`
-        <div class="row mb-3" style="padding-right: 15px; padding-left: 15px;"></div>
-    `)
-    for (let i = 0; i < 9; i++) {
-        $(".row").append(`
-            <div id=${i} onClick="clickImageHandler(this.id)" class="col-xs-4 themed-grid-col">
-                <img src="images/empty-cell.jpg" class="img-responsive center-block">
-            </div>
-        `)
-    }
+    if (gameState === "IN_GAME") return;
 
+    if (gameState === "NO_GAME") {
+        initializeCells();
+    } else if (gameState === "END_GAME") {
+        resetCells();
+    }
+    gameState = "IN_GAME";
+    errorFlag = false;
+    connectSocket();
+}
+
+function connectSocket() {
     socketConnection.subscribe({
-                       next: msg =>handleNext(msg), // Called whenever there is a message from the server.
-                       error: err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
-                       complete: () => console.log('complete') // Called when connection is closed (for whatever reason).
-                      });
+        next: msg => handleNext(msg), // Called whenever there is a message from the server.
+        error: err => handleError(err), // Called if at any point WebSocket API signals some kind of error.
+        complete: () => handleComplete() // Called when connection is closed (for whatever reason).
+    });
 }
 
 function clickImageHandler(clicked_id) {
@@ -55,6 +42,42 @@ function handleNext(msg) {
         $(`#${msg.text} .img-responsive`).attr("src",`images/${figure}.jpg`);
     } else if (msg.type === "INFO") {
         console.log("Got an info!")
-        $("h2").text(msg.text);
+        $("h2").html(msg.text);
+    }
+}
+
+function handleError(err) {
+    console.log(err);
+    if (errorFlag) return;
+    errorFlag = true
+    gameState = "END_GAME"
+    setTimeout(() => {
+        if (errorFlag) {
+            $("h2").html("Ops, something went wrong!<br><br>Tap here to play again!");
+        }
+    }, 10000);
+}
+
+function handleComplete() {
+    console.log('complete');
+    gameState = "END_GAME"
+}
+
+function initializeCells() {
+    $(".container").append(`
+        <div class="row mb-3" style="padding-right: 15px; padding-left: 15px;"></div>
+    `)
+    for (let i = 0; i < 9; i++) {
+        $(".row").append(`
+            <div id=${i} onClick="clickImageHandler(this.id)" class="col-xs-4 themed-grid-col">
+                <img src="images/empty-cell.jpg" class="img-responsive center-block">
+            </div>
+        `)
+    }
+}
+
+function resetCells() {
+    for (let i = 0; i < 9; i++) {
+        $(`#${i} .img-responsive`).attr("src",`images/empty-cell.jpg`);
     }
 }
