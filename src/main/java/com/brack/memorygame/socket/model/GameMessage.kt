acbd54f3f66b2493f3826f47837b7eebcd4ed5b0
brack <angelo.gaming.93@outlook.it>
2024-04-15 15:01:35 +0200

    feat: hide isLast property from json response

--- src/main/java/com/brack/memorygame/socket/model/GameMessage.kt
+++ src/main/java/com/brack/memorygame/socket/model/GameMessage.kt
@@ -1,5 +1,6 @@
 package com.brack.memorygame.socket.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -12,6 +13,7 @@ data class GameMessage(
     val type: MessageType
 ) {
     fun write() : String = mapper.writeValueAsString(this)
+    @JsonIgnore
     fun isLast() : Boolean = this == WIN_MESSAGE || this == LOST_MESSAGE || this == TIE_MESSAGE
 
     companion object {
